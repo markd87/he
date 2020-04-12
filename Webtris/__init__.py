a4e53969800c9c7deda0fa8889dd74e2c35bb407
Mark Danovich <mark.danovich@gmail.com>
2020-04-12 22:59:53 +0100

    added util functions for plotting and showing on map

---
+++ Webtris/__init__.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+import logging
+
+logger = logging.getLogger()
+
+
+class MIDAS:
+    def __init__(self, version=1.0):
+        self.version = version
+        self.base_url = f"http://webtris.highwaysengland.co.uk/api/v{version}"
+        self.gpd_sites = gpd.GeoDataFrame()
+
+    def sites(self) -> gpd.GeoDataFrame:
+        """
+        Get MIDAS sites
+        """
+        url = f"{self.base_url}/sites"
+        res = requests.get(url).json()
+        sites = res["sites"]
+        df = pd.DataFrame(sites)
+        # Additional filtering on the sites
+        # keep only active and with a name
+        df = (
+            df.query("Status != 'Inactive'").query("Name != ''").copy()
+        ).copy()  # keep only active
+        # get type MIDAS/.../
+        df["type"] = df["Name"].apply(lambda x: x.split()[0].strip())
+        # keep only MIDAS
+        df = df.query("type=='MIDAS'").copy()
+        # get link
+        df["link"] = df["Name"].apply(lambda x: x.split(";")[0].split()[-1].strip())
+        # get direction
+        df["direction"] = df["Name"].apply(lambda x: x.split(";")[-1])
+        # create point Geometry
+        df["geometry"] = df.apply(
+            lambda x: Point(x["Longitude"], x["Latitude"]), axis=1
+        )
+        gdf_sites = gpd.GeoDataFrame(df)
+        # set CRS
+        gdf_sites.crs = "epsg:4326"
+        # convert to UK CRS
+        gdf_sites.to_crs("epsg:27700")
+
+        return gdf_sites
+
+    def areas(self) -> pd.DataFrame:
+        """
+        Get MIDAS areas
+        """
+        url = f"{self.base_url}/areas"
+        res = requests.get(url).json()
+        areas = res["areas"]
+        df = pd.DataFrame(areas)
+        return df
+
+    def daily_report(self, start: str, end: str, sites: str) -> pd.DataFrame:
+        """
+        Get daily report between dates for sites
+        """
+        # parse dates
+        try:
+            start = pd.to_datetime(start, dayfirst=True).strftime("%d%m%Y")
+            end = pd.to_datetime(end, dayfirst=True).strftime("%d%m%Y")
+        except Exception:
+            logger.error("Invalid dates")
+        report_df = pd.DataFrame()
+        page = 1
+        url = f"{self.base_url}/reports/Daily?sites={sites}&start_date={start}&end_date={end}&page={page}&page_size=100"
+        res = requests.get(url)
+        # get pages while exist
+        while res.status_code == 200:
+            res_json = res.json()
+            df = pd.DataFrame(res_json["Rows"])
+            report_df = pd.concat([report_df, df])
+            page += 1
+            url = f"{self.base_url}/reports/Daily?sites={sites}&start_date={start}&end_date={end}&page={page}&page_size=100"
+            res = requests.get(url)
+
+        report_df = report_df.drop_duplicates(
+            subset=["Site Name", "Report Date", "Time Period Ending"], keep="first",
+        )
+
+        if len(report_df) == 0:
+            logger.warning("Empty Dataframe")
+            return None
+
+        # convert to datetime
+        report_df["Report Date"] = pd.to_datetime(report_df["Report Date"])
+
+        # create timestamp
+        report_df["time"] = pd.to_datetime(
+            report_df["Report Date"].dt.date.astype(str)
+            + " "
+            + report_df["Time Period Ending"]
+        )
+
+        report_df = report_df.replace("", 0)
+        report_df["Total Volume"] = pd.to_numeric(report_df["Total Volume"])
+        report_df["Avg mph"] = pd.to_numeric(report_df["Avg mph"])
+
+        return report_df.reset_index(drop=True)
---
+++ Webtris/utils.py
@@ -0,0 +1,46 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import geopandas as gpd
+import contextily as ctx
+import plotly.express as px
+
+
+def plot_report(df_report: pd.DataFrame) -> None:
+    """
+    Plot time series of the report dataframe obtained from Webtris API
+    """
+    _, ax = plt.subplots(figsize=(15, 4))
+    sns.lineplot(data=df_report, x="time", y="Total Volume", hue="Site Name")
+    ax.set_title(df_report["Site Name"].values[0], fontsize=18)
+    ax.set_xlabel("Time", fontsize=16)
+    ax.set_ylabel("Total Volume", fontsize=16)
+    ax.tick_params(axis="both", labelsize=15)
+
+
+def show_sites(df_sites: pd.DataFrame, backend="geopandas") -> None:
+    """
+    Show midas sites on a map
+    """
+    if backend == "geopandas":
+        if "geometry" in df_sites.columns:
+            _, ax = plt.subplots(figsize=(10, 10))
+            df_sites.to_crs("epsg:3857").plot(ax=ax, edgecolor="w", alpha=1)
+            ax.grid(False)
+            ctx.add_basemap(ax, url=ctx.providers.Stamen.TonerLite)
+        else:
+            print("no geometry column")
+    elif backend == "mapbox":
+        mapbox_access_token = open(".mapbox").read()
+        px.set_mapbox_access_token(mapbox_access_token)
+        fig = px.scatter_mapbox(
+            df_sites.dropna(),
+            lat="Latitude",
+            lon="Longitude",
+            size_max=15,
+            zoom=10,
+            width=1000,
+            height=550,
+            hover_data=["Name"],
+        )
+        fig.show()
